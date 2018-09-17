65cf91bea86735b51bb6ff8729baba63d6f7a0ad
Dmitry Galkin <mon4ter@gmail.com>
2018-09-17 19:01:16 +0600

    Release 0.1.0

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='fashionable',
+    version='0.1.0',
+    packages=['fashionable'],
+    package_dir={'': 'src'},
+    url='https://github.com/mon4ter/fashionable',
+    license='MIT',
+    author='Dmitry Galkin',
+    author_email='mon4ter@gmail.com',
+    description='Decorate your project with some fashionable supermodels'
+)
