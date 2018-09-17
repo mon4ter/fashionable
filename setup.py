f89200a7127226c5c62f2ea3633d034ef9bb6adb
Dmitry Galkin <mon4ter@gmail.com>
2018-09-17 19:44:24 +0600

    Release 0.2.0

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
+import re
+
 from setuptools import setup
+
+_version_re = re.compile(r"__version__ = '(.*)'")
+
+with open('src/fashionable/__init__.py') as f:
+    version = str(_version_re.search(f.read()).group(1))
 
 setup(
     name='fashionable',
-    version='0.1.0',
+    version=version,
     packages=['fashionable'],
     package_dir={'': 'src'},
     url='https://github.com/mon4ter/fashionable',
